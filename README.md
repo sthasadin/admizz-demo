f36be6bb8e9f2a71bd1e64ccdd5e1979ceeeadac
bishap <bbishap@gmail.com>
2020-12-03 13:17:35 +0545

    implements linting, pre-commit with husky for fail fast and early

--- README.md
+++ README.md
@@ -2,7 +2,14 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First install all the required  dependencies 
+```bash 
+yarn 
+# or 
+npm
+```
+
+Now, run the development server:
 
 ```bash
 npm run dev
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Admizz</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Admizz - Home</title>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
-
-      <main className={styles.main}>
-        xxx
-      </main>
-
-      <footer className={styles.footer}>
-       
-      </footer>
+      <main className={styles.main}>xxx</main>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }

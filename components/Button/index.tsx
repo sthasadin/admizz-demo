b188d8a52834b88160acfca62922fdce551a6e69
bishap <bbishap@gmail.com>
2020-12-06 16:52:15 +0545

    implementation for components

---
+++ components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Button as ButtonComponent} from "@material-ui/core"
+
+const Button = () => {
+    return (
+        <ButtonComponent/>
+    )
+}
+export {Button}

13b96260e7adb51e7bbfbd6c9ff58f17cf7d6479
bishap <bbishap@gmail.com>
2020-12-20 08:00:59 +0545

    implements next config file

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// const withTM = require("next-transpile-modules")(["shared"]);
+const withPlugins = require("next-compose-plugins");
+const withImages = require("next-images");
+// const withSourceMaps = require('@zeit/next-source-maps')();
+module.exports = withPlugins([withImages], {
+  // module.exports = withPlugins([withTM(), withImages], {
+
+//   distDir: "../../dist/functions/.next_admin",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});

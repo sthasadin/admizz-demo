eeba48b72a3b2484c78c5555139f57e3ee16360e
Luckey <luckeysherpa07@gmail.com>
2021-01-03 23:51:22 +0545

    added margins in the card of the BlogDetail

--- components/BlogDetailMember/index.tsx
+++ components/BlogDetailMember/index.tsx
@@ -10,9 +10,90 @@ const BlogDetailMember = () => {
         </div>
       </div>
       <hr />
-      
+      <div className="blog-detail-member__memberList">
+        <div className="memberPost">
+          <div className="blog-detail-member__inner">
+            <div className="blog-detail-member__catagoryContainer">
+              <div className="blog-detail-member__category">technology</div>
+            </div>
+            <div className="blog-detail-member__details">
+              <div className="blogCard__meta">
+                <div className="blogCard__author">Stecy James</div>
+                <div className="blogCard__time">5 min read</div>
+              </div>
+              <div className="blogCard__title">
+                How I got my job in Google with the help of Admizz
+              </div>
+              <div className="blogCard__desc">
+                Get the right career advice for you and earn your best career
+                certificates.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog-detail-member__memberPost">
+          <div className="blog-detail-member__inner">
+            <div className="blog-detail-member__catagoryContainer">
+              <div className="blog-detail-member__category">technology</div>
+            </div>
+            <div className="blog-detail-member__details">
+              <div className="blogCard__meta">
+                <div className="blogCard__author">Stecy James</div>
+                <div className="blogCard__time">5 min read</div>
+              </div>
+              <div className="blogCard__title">
+                How I got my job in Google with the help of Admizz
+              </div>
+              <div className="blogCard__desc">
+                Get the right career advice for you and earn your best career
+                certificates.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog-detail-member__memberPost">
+          <div className="blog-detail-member__inner">
+            <div className="blog-detail-member__catagoryContainer">
+              <div className="blog-detail-member__category">technology</div>
+            </div>
+            <div className="blog-detail-member__details">
+              <div className="blogCard__meta">
+                <div className="blogCard__author">Stecy James</div>
+                <div className="blogCard__time">5 min read</div>
+              </div>
+              <div className="blogCard__title">
+                How I got my job in Google with the help of Admizz
+              </div>
+              <div className="blogCard__desc">
+                Get the right career advice for you and earn your best career
+                certificates.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blog-detail-member__memberPost">
+          <div className="blog-detail-member__inner">
+            <div className="blog-detail-member__catagoryContainer">
+              <div className="blog-detail-member__category">technology</div>
+            </div>
+            <div className="blog-detail-member__details">
+              <div className="blogCard__meta">
+                <div className="blogCard__author">Stecy James</div>
+                <div className="blogCard__time">5 min read</div>
+              </div>
+              <div className="blogCard__title">
+                How I got my job in Google with the help of Admizz
+              </div>
+              <div className="blogCard__desc">
+                Get the right career advice for you and earn your best career
+                certificates.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}; 
+};
 
 export { BlogDetailMember };

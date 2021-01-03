b3874a0ffc8d8b40e704cd63a445954bc627ef29
Luckey <luckeysherpa07@gmail.com>
2021-01-04 02:45:09 +0545

    addded props to Singleblogcard

--- components/BlogListMember/index.tsx
+++ components/BlogListMember/index.tsx
@@ -6,14 +6,32 @@ const BlogListMember = () => {
     <div className="blog-list-member">
       <div className="blog-list-member__highlightPost">
         <div className="blog-list-member__topPost">
-          <SingleBlogCard />
+          <SingleBlogCard
+            type="Technology"
+            auther="Stecy James"
+            time="5 min read"
+            title="How I got my job in Google with the help of Admizz"
+            desc="Get the right career advice for you and earn your best career certificates."
+          />
         </div>
         <div className="blog-list-member__secondaryPostContainer">
           <div className="blog-list-member__secondaryPost">
-          <SingleBlogCard />
+            <SingleBlogCard
+              type="Business"
+              auther="David Hoffman"
+              time="5 min read"
+              title="How I got my job in Google with the help of Admizz"
+              desc="Get the right career advice for you and earn your best career certificates."
+            />
           </div>
           <div className="blog-list-member__secondaryPost">
-          <SingleBlogCard />
+            <SingleBlogCard
+              type="Colleges"
+              auther="Quicy Sean"
+              time="5 min read"
+              title="How I got my job in Google with the help of Admizz"
+              desc="Get the right career advice for you and earn your best career certificates."
+            />
           </div>
         </div>
       </div>
--- components/SingleBlogCard/index.tsx
+++ components/SingleBlogCard/index.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 
-const SingleBlogCard = () => {
+const SingleBlogCard = (props: any) => {
   return (
     <div className="single-blog-card">
       <div className="single-blog-card__memberPost">
         <div className="single-blog-card__inner">
           <div className="single-blog-card__catagoryContainer">
-            <div className="single-blog-card__category">technology</div>
+            <div className="single-blog-card__category">{props.type}</div>
           </div>
           <div className="single-blog-card__details">
             <div className="blogCard__meta">
-              <div className="blogCard__author">Stecy James</div>
-              <div className="blogCard__time">5 min read</div>
+              <div className="blogCard__author">{props.auther}</div>
+              <div className="blogCard__time">{props.time}</div>
             </div>
             <div className="blogCard__title">
-              How I got my job in Google with the help of Admizz
-              </div>
+              {props.title}
+            </div>
             <div className="blogCard__desc">
-              Get the right career advice for you and earn your best career
-              certificates.
-              </div>
+              {props.desc}
+            </div>
           </div>
         </div>
       </div>

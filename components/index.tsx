1063f4a3a9a9880ac937e1d388798def38bef53d
thinleyN <thinleytyrant@gmail.com>
2021-01-03 23:52:37 +0545

    adds datepicker calender design

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,4 @@ export { Button } from "./Button";
 export * from "./Select";
 export * from "./ContactForm"
 export * from "./BookCounselling"
+export * from "./Datepicker"
---
+++ pages/free-counseling/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Datepicker } from "../../components";
+import { Navbar } from "../../layouts/navbar";
+
+const FreeCounseling = () => {
+  return (
+    <div>
+      <Navbar />
+      <Datepicker />
+    </div>
+  );
+};
+
+export default FreeCounseling;

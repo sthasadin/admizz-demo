ad42dbe15167409e4be71b27dab54de286d605cc
Luckey <luckeysherpa07@gmail.com>
2021-01-04 02:39:32 +0545

    added singleBlogPost in Project

--- components/BlogListMember/index.tsx
+++ components/BlogListMember/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { SingleBlogCard } from "../SingleBlogCard";
 
 const BlogListMember = () => {
   return (
     <div className="blog-list-member">
       <div className="blog-list-member__highlightPost">
         <div className="blog-list-member__topPost">
-          Top Post
+          <SingleBlogCard />
         </div>
         <div className="blog-list-member__secondaryPostContainer">
           <div className="blog-list-member__secondaryPost">
-            SecondaryPost One
+          <SingleBlogCard />
           </div>
           <div className="blog-list-member__secondaryPost">
-            SecondaryPost Two
+          <SingleBlogCard />
           </div>
         </div>
       </div>

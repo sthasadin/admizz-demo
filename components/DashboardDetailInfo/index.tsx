85e28ad27278c10fd75a8138a924ea16e65b6780
Luckey <luckeysherpa07@gmail.com>
2021-01-05 07:11:30 +0545

    added Dashboard Detail

--- components/DashboardDetailInfo/index.tsx
+++ components/DashboardDetailInfo/index.tsx
@@ -7,7 +7,7 @@ const DashboardDetailInfo = () => {
       {/* <Basic Card Info */}
       <div className="dashboard-detail-info__detailCard">
         <div className="dashboard-detail-info__detailCardTitle">
-          Basic Info
+          Basic Information
         </div>
         <div className="dashboard-detail-info__detailCardInfo">
           <div className="dashboard-detail-info__cardRow">
@@ -58,24 +58,120 @@ const DashboardDetailInfo = () => {
               kathmandu
             </div>
           </div>
-          <div className="dashboard-detail-info__cardRow">
-            <div className="dashboard-detail-info__keyText">
-              Permanent Address
+          <div className="dashboard-detail-info__cardRowButton">
+            <div className="dashboard-detail-info__viewText">
+              View
             </div>
-            <div className="dashboard-detail-info__valueText">
-              Kathmandu
+            <div className="dashboard-detail-info__editText">
+              Edit
             </div>
           </div>
         </div>
       </div>
+
+      {/* <Background Information */}
       <div className="dashboard-detail-info__detailCard">
         <div className="dashboard-detail-info__detailCardTitle">
-          Basic Info
+          Background Information
+        </div>
+        <div className="dashboard-detail-info__detailCardInfo">
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              Personal Detail
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              Citizenship No. 
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              987654321
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              References
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__valueText">
+              Ram B. Mahat
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              987654321
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              987654321
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__valueText">
+              Ram B. Mahat
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              987654321
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              987654321
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRowButton">
+            <div className="dashboard-detail-info__viewText">
+              View
+            </div>
+            <div className="dashboard-detail-info__editText">
+              Edit
+            </div>
+          </div>
         </div>
       </div>
+
+      {/* <Choice Detail */}
       <div className="dashboard-detail-info__detailCard">
         <div className="dashboard-detail-info__detailCardTitle">
-          Basic Info
+          Choice Detail
+        </div>
+        <div className="dashboard-detail-info__detailCardInfo">
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              Level
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              Under Graduate
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              Stream  
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              Engineering, Management, Humanities
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              Specialization
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              Computer Science, Electronics, Medical Science
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRow">
+            <div className="dashboard-detail-info__keyText">
+              College/University
+            </div>
+            <div className="dashboard-detail-info__valueText">
+              Thaper, JIT, KIIT, BMS Engineering, Other
+            </div>
+          </div>
+          <div className="dashboard-detail-info__cardRowButton">
+            <div className="dashboard-detail-info__viewText">
+              View
+            </div>
+            <div className="dashboard-detail-info__editText">
+              Edit
+            </div>
+          </div>
         </div>
       </div>
     </div>

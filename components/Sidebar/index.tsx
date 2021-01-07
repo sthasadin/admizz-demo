3ce0af59c81c993a3dec93cce2edfba8be949adc
binaya346 <rijalbinaya2@gmail.com>
2021-01-07 13:39:46 +0545

    Adds faculty information

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { CallToAction } from "../Button/callToAction";
+import FacultyInformation from "../FacultyInformation";
 
 const Sidebar = (props: any) => {
   return (
     <aside className="sidebar">
       <CallToAction className="filled full-width">Apply Now</CallToAction>
+      <div className="sidebar__cta">
+        <CallToAction className="transparent full-width">
+          Add to Compare
+        </CallToAction>
+      </div>
+      <FacultyInformation />
     </aside>
   );
 };

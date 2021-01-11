5106e58e5707bd119c92c9f799b6fce247b10082
Luckey <luckeysherpa07@gmail.com>
2021-01-11 14:55:46 +0545

    added form input in the page

--- components/DashboardBasicInfo/index.tsx
+++ components/DashboardBasicInfo/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Select } from "../Select";
 import { Input } from "../Input";
+import { Grid } from '@material-ui/core';
+import { Button } from '../Button';
 
 const DashboardBasicInfo = () => {
   return (
@@ -10,10 +12,10 @@ const DashboardBasicInfo = () => {
         <div className="dashboard-basic-info__pageTitle">
           Dashboard > Apply
         </div>
-        <div className="dashboard-basic-info__sectionTitle"> 
+        <div className="dashboard-basic-info__sectionTitle">
           Applying For
         </div>
-        <div className ="dashboard-basic-info__formContainer">
+        <div className="dashboard-basic-info__formContainer">
           <div className="dashboard-basic-info__inputContainer">
             <Select title="Select Stream" />
           </div>
@@ -25,19 +27,151 @@ const DashboardBasicInfo = () => {
 
       {/* Personal Detail */}
       <div className="dashboard-basic-info__sectionContainer">
-        <div className="dashboard-basic-info__sectionTitle"> 
+        <div className="dashboard-basic-info__sectionTitle">
           Personal Detail
         </div>
-        <div className ="dashboard-basic-info__formContainer">
-          <div className="dashboard-basic-info__inputContainerSmall">
-            <Input placeholder={"Full Name"} fullWidth margin={"25px 0px 16px 0px"} />
+        <div className="dashboard-basic-info__formContainer">
+          <form>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Full Name" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Middle Name" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Last Name" />
+              </Grid>
+            </Grid>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <div className={'student-info__phone-input'}>
+                  <div className={'student-info__phone-separator'}>
+                    +977
+                  </div>
+                  <Input
+                    className={'student-info__input student-info__phone'}
+                    fullWidth
+                    placeholder="Phone Number" />
+                </div>
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Email Address" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="Nationality" />
+              </Grid>
+            </Grid>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'student-info__input student-info__phone'}
+                  fullWidth
+                  placeholder="Date Of Birth" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="Gender" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </div>
+
+      {/* Address Detail */}
+      <div className="dashboard-basic-info__sectionContainer">
+        <div className="dashboard-basic-info__sectionTitle">
+          Address Detail
+        </div>
+        <div className="dashboard-basic-info__formContainer">
+          <div className="dashboard-basic-info__formTitle">
+            Residential Address
           </div>
-          <div className="dashboard-basic-info__inputContainerSmall">
-            <Input placeholder={"Middle Name"} fullWidth margin={"25px 0px 16px 0px"} />
+          <form>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Address Line 1" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="Country" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="State" />
+              </Grid>
+            </Grid>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="City" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Zip Code" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <div className="dashboard-basic-info__formContainer">
+          <div className="dashboard-basic-info__formTitle">
+            Permanent Address
           </div>
-          <div className="dashboard-basic-info__inputContainerSmall">
-            <Input placeholder={"Last Name"} fullWidth margin={"25px 0px 16px 0px"} />
+          <form>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Address Line 1" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="Country" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="State" />
+              </Grid>
+            </Grid>
+            <Grid container className="dashboard-basic-info__row" justify="space-around" direction='row' >
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Select title="City" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+                <Input
+                  className={'dashboard-basic-info__input'}
+                  fullWidth
+                  placeholder="Zip Code" />
+              </Grid>
+              <Grid className={'dashboard-basic-info__grid'} item sm={12} md={4}>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <div className="dashboard-basic-info__buttonContainer">
+          <div className="dashboard-basic-info__backContainer">
+              Back
           </div>
+          <Button
+              onClick={console.log("This is the value")}>
+              Continue
+          </Button>
         </div>
       </div>
     </div>

8846c83805fe7ef346f536852749b28bff1e7c25
Luckey <luckeysherpa07@gmail.com>
2021-01-29 20:50:23 +0545

    fixing firestore

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import * as yup from "yup";
+import { auth } from "../../firebase";
 import indiaOffice from "../../public/indiaOffice.png"
 import nepalOffice from "../../public/nepalOffice.png"
 import location from "../../public/location.png"
@@ -15,89 +17,134 @@ import SubjectIcon from '@material-ui/icons/Subject';
 import LiveHelpIcon from '@material-ui/icons/LiveHelp';
 
 const ContactForm = () => {
-    return (
-        <div className="contact-form">
-            <div className="contact-form__contact-us">
-                <div className="contact-form__header">
-                    Contact Details
+  const [formValue, setFormValue] = useState({} as ContactUsFormValue);
+  const [formError, setFormError] = useState({} as any);
+
+  const handleChange = (e: any) => {
+    console.log("THIS IS THE VALUE", e.target.name, e.target.value, formValue)
+    setFormValue({ ...formValue, [e.target.name]: e.target.value })
+  }
+
+  const validationSchema = yup.object().shape<ContactUsFormValue>({
+    name: yup.string().required("Required"),
+    email: yup
+      .string()
+      .required("Required")
+      .email("Please provide a valid email"),
+    subject: yup.string().required("Required"),
+    query: yup.string().required("Required")
+  });
+
+  const validate = async () => {
+    try {
+      await validationSchema.validate({
+        name: formValue.name,
+        email: formValue.email,
+        subject: formValue.subject,
+        query: formValue.query
+      }, {
+        abortEarly: false
+      });
+      return false;
+    } catch (err) {
+      const errors = {};
+      err.inner.forEach((item: any) => {
+        errors[item.path] = item.errors[0]
+      })
+      setFormError({ ...errors })
+    }
+  }
+
+  const handleRegister = async () => {
+    const valid = await validate();
+    if (valid) {
+      const res = await auth.createUserWithEmailAndPassword(formValue.email, formValue.password);
+    }
+  }
+
+  return (
+    <div className="contact-form">
+      <div className="contact-form__contact-us">
+        <div className="contact-form__header">
+          Contact Details
                 </div>
-                <div>
-                    <div className="contact-form__india-office-container">
-                        <img src={indiaOffice} />
-                        <div className="contact-form__office-details">
-                            <div className="contact-form__details-header">
-                                India Office
+        <div>
+          <div className="contact-form__india-office-container">
+            <img src={indiaOffice} />
+            <div className="contact-form__office-details">
+              <div className="contact-form__details-header">
+                India Office
                         </div>
-                            <div className="contact-form__details-content">
-                                <img src={location} />
-                                <span>
-                                    Bangalore,<br />Karnataka, India
+              <div className="contact-form__details-content">
+                <img src={location} />
+                <span>
+                  Bangalore,<br />Karnataka, India
                           </span>
-                            </div>
-                            <div className="contact-form__details-content">
-                                <img src={telephone} />
-                                <span>
-                                    +91 805 0259 693
+              </div>
+              <div className="contact-form__details-content">
+                <img src={telephone} />
+                <span>
+                  +91 805 0259 693
                           </span>
-                            </div>
+              </div>
+            </div>
+          </div>
+          <div className="contact-form__india-office-container">
+            <img src={nepalOffice} />
+            <div className="contact-form__office-details">
+              <div className="contact-form__details-header">
+                Nepal Office
                         </div>
-                    </div>
-                    <div className="contact-form__india-office-container">
-                        <img src={nepalOffice} />
-                        <div className="contact-form__office-details">
-                            <div className="contact-form__details-header">
-                                Nepal Office
-                        </div>
-                            <div className="contact-form__details-content">
-                                <img src={location} />
-                                <span>
-                                    Bagbazar,<br />Kathmandu, Nepal
+              <div className="contact-form__details-content">
+                <img src={location} />
+                <span>
+                  Bagbazar,<br />Kathmandu, Nepal
                           </span>
-                            </div>
-                            <div className="contact-form__details-content">
-                                <img src={telephone} />
-                                <span>
-                                    +91 805 0259 693
+              </div>
+              <div className="contact-form__details-content">
+                <img src={telephone} />
+                <span>
+                  +91 805 0259 693
                           </span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="contact-form__email-us">
-                        Prefer Email? Email us at:
+              </div>
+            </div>
+          </div>
+          <div className="contact-form__email-us">
+            Prefer Email? Email us at:
                         <div className="contact-form_email-holder">
-                            <img src={email} />
-                            info@linkservices.co
+              <img src={email} />
+              info@linkservices.co
                         </div>
-                    </div>
-                </div>
-            </div>
-            <div className="contact-form__lets-talk">
-                <div className="contact-form__header">
-                    Let's Talk
-                 </div>
-                <div className="contact-form__message">
-                    Please tell us a bit about you, your project, and how best to reach you. We’ll get right back to you.
-                 </div>
-                <Input placeholder={"Full Name"} fullWidth margin={"25px 0px 16px 0px"} icon={PersonIcon} />
-                <Input placeholder={"Email Address"} margin={"0px 0px 16px 0px"} fullWidth icon={MailIcon} />
-                <Input placeholder={"Subject"} margin={"0px 0px 16px 0px"} fullWidth icon={SubjectIcon} />
-                <Input multiline placeholder={"Add additional query you have"} margin={"0px 0px 16px 0px"} fullWidth icon={LiveHelpIcon}/>
-                <div className="contact-form__actions">
-                    <div className="contact-form__socials">
-                        <div>Follow Us on:</div>
-                        <div className="contact-form__icons">
-                            <img src={Facebook} width="11px" height="23px" />
-                            <TwitterIcon style={{ fill: "#2F80ED", marginRight: 47, marginLeft: 47 }} />
-                            <InstagramIcon style={{ fill: "#2F80ED" }} />
-                        </div>
-                    </div>
-                    <div className="contact-form__submit">
-                        <Button className="contact-form__send-message" >Send Message</Button>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="contact-form__lets-talk">
+        <div className="contact-form__header">
+          Let's Talk
+                 </div>
+        <div className="contact-form__message">
+          Please tell us a bit about you, your project, and how best to reach you. We’ll get right back to you.
+                 </div>
+        <Input name={"name"} onChange={handleChange} placeholder={"Full Name"} fullWidth margin={"25px 0px 16px 0px"} icon={PersonIcon} />
+        <Input name={"email"} onChange={handleChange} placeholder={"Email Address"} margin={"0px 0px 16px 0px"} fullWidth icon={MailIcon} />
+        <Input name={"subject"} onChange={handleChange} placeholder={"Subject"} margin={"0px 0px 16px 0px"} fullWidth icon={SubjectIcon} />
+        <Input name={"query"} onChange={handleChange} multiline placeholder={"Add additional query you have"} margin={"0px 0px 16px 0px"} fullWidth icon={LiveHelpIcon} />
+        <div className="contact-form__actions">
+          <div className="contact-form__socials">
+            <div>Follow Us on:</div>
+            <div className="contact-form__icons">
+              <img src={Facebook} width="11px" height="23px" />
+              <TwitterIcon style={{ fill: "#2F80ED", marginRight: 47, marginLeft: 47 }} />
+              <InstagramIcon style={{ fill: "#2F80ED" }} />
+            </div>
+          </div>
+          <div className="contact-form__submit">
+            <Button onClick={handleRegister} className="contact-form__send-message" >Send Message</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export { ContactForm };

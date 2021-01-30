46b0e8cd821f3d1ab46bddb84d2869a61004f833
Luckey <luckeysherpa07@gmail.com>
2021-01-30 16:30:01 +0545

    added firestore to the Counsellor Stepper

--- components/CounselingStepper/SelectCounseler.tsx
+++ components/CounselingStepper/SelectCounseler.tsx
@@ -1,61 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import counseler from "../../public/counseler.png";
 import counselerImage from "../../public/counselerImage.png";
+import { db } from '../../firebase';
 
 const SelectCounseler = () => {
-    const counseletList = [
-        {
-            id: 1,
-            image: counselerImage,
-            name: 'Ramos Ali',
-            nationality: "Nepal"
-        },
-        {
-            id: 2,
-            image: counselerImage,
-            name: 'Ramos Ali',
-            nationality: "Nepal"
-        }, {
-            id: 3,
-            image: counselerImage,
-            name: 'Ramos Ali',
-            nationality: "Nepal"
-        },
-        {
-            id: 4,
-            image: counselerImage,
-            name: 'Ramos Ali',
-            nationality: "Nepal"
-        }
-    ]
-    const [selectedCounseler, setSelectedCounseler] = useState(1 as number)
-    return (
-        <div className="select-counseler">
-            <div className="select-counseler__header">
-                <img src={counseler} className="select-counser__counseler-icon" />
-                <div>Select Counseler</div>
-            </div>
-            <div className="select-counseler__counseler-list">
-                {counseletList.map(item => {
-                    return (
-                        <div onClick={() => setSelectedCounseler(item.id)}
-                            className={`select-counseler__counseler-card`}>
-                            <img src={item.image} />
-                            <div className={`select-counseler__counseler-info 
+
+  const [counsellorArray, setCounsellorArray] = useState([]);
+
+  const getFireStoreCounselor = async () => {
+    const counsellor = []
+    await db.collection("counsellor").get().then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+        const data = doc.data()
+        counsellor.push({
+          name: data.name,
+          country: data.country
+        })
+      });
+    });
+    setCounsellorArray([...counsellor])
+  }
+
+  useEffect(() => {
+    getFireStoreCounselor();
+  }, [])
+
+  const [selectedCounseler, setSelectedCounseler] = useState(1 as number)
+  return (
+    <div className="select-counseler">
+      <div className="select-counseler__header">
+        <img src={counseler} className="select-counser__counseler-icon" />
+        <div>Select Counseler</div>
+      </div>
+      <div className="select-counseler__counseler-list">
+        {counsellorArray.map(item => {
+          return (
+            <div onClick={() => setSelectedCounseler(item.id)}
+              className={`select-counseler__counseler-card`}>
+              <img src={item.image} />
+              <div className={`select-counseler__counseler-info 
                                  ${selectedCounseler === item.id && 'select-counseler__counseler-info-selected'}`}>
-                                <div className="select-counseler__counseler-name">
-                                    {item.name}
-                                </div>
-                                <div className="select-counseler__counseler-nationaliy">
-                                    {item.nationality} Counseler
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })}
+                <div className="select-counseler__counseler-name">
+                  {item.name}
+                </div>
+                <div className="select-counseler__counseler-nationaliy">
+                  {item.nationality} Counseler
+                </div>
+              </div>
             </div>
-        </div>
-    );
+          )
+        })}
+      </div>
+    </div>
+  );
 };
 
 export { SelectCounseler };

e365dd2ea4364db4714df8a9bbe1c06b36feb72a
thinleyN <thinleytyrant@gmail.com>
2021-01-30 15:34:50 +0545

    fix loader position of private and restricted route

--- pages/withRestrictedRoute.tsx
+++ pages/withRestrictedRoute.tsx
@@ -1,26 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import Router from "next/router";
-import { createUseStyles } from "react-jss";
 import CircularProgress from "@material-ui/core/CircularProgress";
-
-const useStyle = createUseStyles(() => {
-  return {
-    loader: {
-      display: "flex",
-      justifyContent: "center",
-      alignContent: "center",
-      height: "100vh",
-      width: "100%",
-    },
-  };
-});
 
 const withRestrictedRoute = (AuthComponent) => {
   function RestrictedComponent({ children }) {
     const { authenticated, loading, user } = useContext(AuthContext);
     const [toRender, setToRender] = useState(false);
-    const classes = useStyle();
     useEffect(() => {
       if (authenticated == null) {
         return;
@@ -33,7 +19,7 @@ const withRestrictedRoute = (AuthComponent) => {
     }, [authenticated, user]);
     if (!toRender || loading) {
       return (
-        <div className={classes.loader}>
+        <div className={"route-load"}>
           <CircularProgress />
         </div>
       );

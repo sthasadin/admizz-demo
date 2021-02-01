5c7d05e03c8bf3b88954ed6ce3d8dca59cbf6e9d
Luckey <luckeysherpa07@gmail.com>
2021-02-01 23:34:19 +0545

    fixed the responsive inner container navbar

--- layouts/navbar.tsx
+++ layouts/navbar.tsx
@@ -6,35 +6,37 @@ const Navbar = () => {
   const router = useRouter();
   return (
     <div className="navbar">
-      <div className="navbar__logo">
-        <div className="logo" onClick={() => router.push('/')}>
-          <img src="/logo.png" alt="" />
+      <div className="navbar__inner">
+        <div className="navbar__logo">
+          <div className="logo" onClick={() => router.push('/')}>
+            <img src="/logo.png" alt="" />
+          </div>
         </div>
-      </div>
-      <div className="navbar__right">
-        <div className="navbar__menu">
-          <nav className="navigation">
-            <ul className="menu">
-              <li className="menu-item">
-                <Link href="/">Home</Link>
-              </li>
-              <li className="menu-item">
-                <Link href="/colleges">Colleges</Link>
-              </li>
-              <li className="menu-item">
-                <Link href="/blogs">Blogs</Link>
-              </li>
-              <li className="menu-item">
-                <Link href="/contact-us">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="navbar__search-icon">
-          <div className="search-icon"></div>
-        </div>
-        <div className="navbar__drawer-icon">
-          <div className="drawer-icon"></div>
+        <div className="navbar__right">
+          <div className="navbar__menu">
+            <nav className="navigation">
+              <ul className="menu">
+                <li className="menu-item">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="menu-item">
+                  <Link href="/colleges">Colleges</Link>
+                </li>
+                <li className="menu-item">
+                  <Link href="/blogs">Blogs</Link>
+                </li>
+                <li className="menu-item">
+                  <Link href="/contact-us">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="navbar__search-icon">
+            <div className="search-icon"></div>
+          </div>
+          <div className="navbar__drawer-icon">
+            <div className="drawer-icon"></div>
+          </div>
         </div>
       </div>
     </div>

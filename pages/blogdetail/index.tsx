3b1dafd0eff49309cf3eba4b8ded3ffa6c7482c2
Luckey <luckeysherpa07@gmail.com>
2021-02-02 10:58:03 +0545

    fixed blogdetail header responsive

--- pages/blogdetail/index.tsx
+++ pages/blogdetail/index.tsx
@@ -46,7 +46,7 @@ const blogDetail = () => {
             <div className="blog-detail__main">
               <BlogDetailHeader />
             </div>
-            <div className="blog-detail__imageContainer">
+            {/* <div className="blog-detail__imageContainer">
               <img className="blog-detail__image" src={BlogImage} />
               <div className="blog-detail__postDetail">
                 <div className="blog-detail__postValueContainer">
@@ -111,13 +111,13 @@ const blogDetail = () => {
               <div className="blog-detail__imageContainer">
                 <BlogDetailMember />
               </div>
-            </div>
-            </div>
+            </div> */}
+          </div>
         </main>
       </div>
-        <Footer />
-      </div>
-      );
-    };
-    
-    export default blogDetail;
+      <Footer />
+    </div>
+  );
+};
+
+export default blogDetail;

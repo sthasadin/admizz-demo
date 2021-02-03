ad53a9daf0097b36911df13aaa99ced2607f9c13
Luckey <luckeysherpa07@gmail.com>
2021-02-04 01:32:35 +0545

    fixed dashboard navbar

--- layouts/dashboardnavbar.tsx
+++ layouts/dashboardnavbar.tsx
@@ -6,32 +6,32 @@ import searchIcon from "./../public/SearchIcon.png";
 const DashboardNavbar = () => {
   return (
     <div className="navbar">
-      <div className="navbar__logo">
-        <div className="logo">
+      <div className="navbar__inner">
+        <div className="navbar__logoImage">
           <img src="/logo.png" alt="" />
         </div>
-      </div>
-      <div className="navbar__right">
-        <div className="navbar__menu">
-          <nav className="navigation">
-            <ul className="menu">
-              <li className="menu-item">
-                <img className="navbar__navImage" src={searchIcon} />
-              </li>
-              <li className="menu-item">
-                <img className="navbar__navImage" src={notificationIcon} />
-              </li>
-              <li className="menu-item">
-                <img src={userImage} />
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="navbar__search-icon">
-          <div className="search-icon"></div>
-        </div>
-        <div className="navbar__drawer-icon">
-          <div className="drawer-icon"></div>
+        <div className="navbar__dashboardRight">
+          <div className="navbar__menu">
+            <nav className="navigation">
+              <ul className="menu">
+                <li className="menu-item">
+                  <img className="navbar__navImage" src={searchIcon} />
+                </li>
+                <li className="menu-item">
+                  <img className="navbar__navImage" src={notificationIcon} />
+                </li>
+                <li className="menu-item">
+                  <img src={userImage} />
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="navbar__search-icon">
+            <div className="search-icon"></div>
+          </div>
+          <div className="navbar__drawer-icon">
+            <div className="drawer-icon"></div>
+          </div>
         </div>
       </div>
     </div>

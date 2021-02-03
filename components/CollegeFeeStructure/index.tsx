e476fdd975b4f697098ef0f2fec8f26a71488bc7
Luckey <luckeysherpa07@gmail.com>
2021-02-03 23:46:07 +0545

    fixed responsive of college fee

--- components/CollegeFeeStructure/index.tsx
+++ components/CollegeFeeStructure/index.tsx
@@ -16,17 +16,19 @@ const FeeStructure = (props: any) => {
             <div className="level-list__item">PH.D</div>
           </div>
         </div>
-        {/* <div className="fee-structure__courses">
+        <div className="fee-structure__courses">
           <div className="courses-list">
             <div className="courses-list__title">All Courses</div>
-            <div className="courses-list__item">Engineering</div>
-            <div className="courses-list__item">Management</div>
-            <div className="courses-list__item">Pharmacy</div>
-            <div className="courses-list__item">Medical</div>
-            <div className="courses-list__item">Tourism</div>
+            <div className="courses-list__itemContainer">
+              <div className="courses-list__item">Engineering</div>
+              <div className="courses-list__item">Management</div>
+              <div className="courses-list__item">Pharmacy</div>
+              <div className="courses-list__item">Medical</div>
+              <div className="courses-list__item">Tourism</div>
+            </div>
           </div>
-        </div> */}
-        {/* <div className="fee-structure__course-fee">
+        </div>
+        <div className="fee-structure__course-fee">
           <div className="course-fee">
             <div className="course-fee__item">
               <div className="course-fee__course">MBA</div>
@@ -88,7 +90,7 @@ const FeeStructure = (props: any) => {
               </a>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );

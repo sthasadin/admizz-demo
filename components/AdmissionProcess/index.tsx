f95e12dc4dbcd0e9f99b043bc13dc1bd94b7afd8
AanandBhandari <aanandbhandari143@gmail.com>
2021-02-28 15:57:58 +0545

    admission process

--- components/AdmissionProcess/index.tsx
+++ components/AdmissionProcess/index.tsx
@@ -1,28 +1,25 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import moment from 'moment'
 import schlorship from "../../public/scholarship.png";
 import { ScholarshipOption } from "./schlorshipOption";
 
 const AdmissionProcess = () => {
+  const admission = useSelector(state => state.college.college?.addmission_process)
   {
     return (
       <div id="admission" className="admission-process">
         <div className="admission-process__inner">
           <div className="admission-process__title">Admission Process</div>
           <div className="admission-process__heading">
-            Easy Admission Process
+            {admission?.title}
           </div>
           <div className="admission-process__start">
-            Application Open for Jan Session:
-            <span>Start Date: 18 Dec, 2020 / End Date: 28 Dec, 2020</span>
+            {/* Application Open for Jan Session: */}
+            <span> Start Date: {moment(admission?.start).format("DD MMM YYYY")} / End Date: {moment(admission?.end).format("DD MMM YYYY")}</span>
           </div>
           <div className="admission-process__desc">
-            by Karam Chand Thapar in 1956. Karam Chand Thapar, one the great
-            captains of Indian Industry established this institute to provide
-            close interaction with industry, and a strong emphasis on basic and
-            applied research. The mission of the institution as embodied in the
-            Trust Deed dated April 9, 1956. It was granted full autonomy and the
-            status of deemed-to-be-university by the University Grants
-            Commission in 1985.
+            {admission?.description}
           </div>
           <div className="admission-process__heading">Scholarship Options</div>
           <div className="admission-process__list">

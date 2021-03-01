235d838131b9b7b7404146f8106871b307a607d0
AanandBhandari <aanandbhandari143@gmail.com>
2021-03-01 17:32:05 +0545

    incompleted qna and review

--- components/CollegeRatingAndReviews/index.tsx
+++ components/CollegeRatingAndReviews/index.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React,{useEffect} from "react";
+import {useSelector,useDispatch} from 'react-redux'
+import { getQnas } from "../../store/Action/review.action";
 import { Review } from "../Review";
 import { RatingItem } from "./ratingItem";
 
+
 const RatingAndReview = (props: any) => {
+  const dispatch = useDispatch()
+  const college_id = useSelector(state => state.college.college._id)
+  useEffect(()=> {
+    dispatch(getQnas(college_id))
+  },[college_id])
+  // const seedReviews = () => {
+  //   var batch = db.batch()
+  //   let array = [{
+  //   type:String,//enum[Academics, Accomodation, Faculty, Infrastructures, Placements, Social]
+  //   // college:college,
+  //   // student:student,
+  //   // comment:String,
+  //   // star:Number,//max 10
+  //   // no_of_likes:[student],
+  //   // no_of_dislikes:[student],
+  //   isReported: Boolean//default false
+  //   }]
+  //   array.forEach((doc) => {
+  //     var docRef = db.collection("review").doc(); //automatically generate unique id
+  //     batch.set(docRef, doc);
+  //   });
+  //   batch.commit().then((data)=>{
+  //     console.log(data)
+  //   }).catch(err => {
+  //     console.log(err)
+  //   })
+
+  // }
+  // useEffect(()=> {
+  // },[db])
+
+  
   return (
     <div id="rating" className="rating-review">
       <div className="rating-review__title-wrap">

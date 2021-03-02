37d2108493d1280949b7562c0e2117f02f485be8
AanandBhandari <aanandbhandari143@gmail.com>
2021-03-02 10:51:31 +0545

    add subscriber on college detail

--- components/Newsletter/index.tsx
+++ components/Newsletter/index.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import * as yup from "yup";
+import {useDispatch} from 'react-redux'
+import { addSubscriber } from "../../store/Action/subscriber.action";
 
 const Newsletter = (props: any) => {
+  const [subscriber, setSubscriber] = useState('')
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  const onChange = e => {
+    setSubscriber(e.target.value)
+  }
+  const onClick =async ()=> {
+    setLoading(true)
+    let schema = yup.string().email();
+    let result = schema.isValidSync(subscriber)
+    if (result) {      
+      await dispatch(addSubscriber(subscriber))
+    }
+    setSubscriber('')
+    setLoading(false)
+  }
   return (
     <div id="newsletter" className="newsletter">
       <div className="newsletter__inner">
         <div className="newsletter__title">Subscribe to our Newsletter</div>
         <div className="newsletter__form">
           <div className="newsletter__input">
-            <input type="text" placeholder="Enter email Address" />
+            <input value={subscriber} onChange={onChange} type="text" placeholder="Enter email Address" />
           </div>
-          <button className="newsletter__btn">Subscribe Now</button>
+          <button onClick={onClick} disabled={loading} className="newsletter__btn">Subscribe Now</button>
         </div>
       </div>
     </div>

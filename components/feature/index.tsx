888699dfec510d5b39cd7ebebfba66598a8bd473
HUMAN <rodinshrestha@gmail.com>
2021-03-14 17:10:06 +0545

    Functionality added

--- components/feature/index.tsx
+++ components/feature/index.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import chatIcon from "../../public/chatIcon.png";
 
-const Feature = () => {
+const Feature = ({ data }) => {
   return (
     <div className="feature">
       <div className="feature__heading">Our Features:</div>
 
       <div className="feature__listcontainer">
         <div className="feature__details">
-          <img src={chatIcon} />
-          <div className="feature__title">One on One Counselling session</div>
+          <img src={data.icon} />
+          <div className="feature__title">{data.title}</div>
         </div>
-        <div className="feature__descrition">
-          Get the right career advice for you and earn your best carer
-          certificates.
-        </div>
+        <div className="feature__descrition">{data.description}</div>
       </div>
 
       <div className="feature__btn">

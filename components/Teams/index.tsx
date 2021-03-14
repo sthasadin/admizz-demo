8d83f6534fca37041426d44e0e26454c140e592b
HUMAN <rodinshrestha@gmail.com>
2021-03-14 20:18:03 +0545

    Dynamic content

--- components/Teams/index.tsx
+++ components/Teams/index.tsx
@@ -1,24 +1,70 @@
 import React from "react";
 import { TeamArray } from "./TeamArray";
+import Link from "next/link";
 import TeamMemberList from "./TeamMemberList";
+import youtubeIcon from "../../public/youtubeIcon.png";
+import instagramIcon from "../../public/instagramIcon.png";
+import facebookIcon from "../../public/facebookIcon.png";
+import twitterIcon from "../../public/twitterIcon.png";
+import mIcon from "../../public/mIcon.png";
+import { db, storage } from "../../firebase";
 
 const Teams = () => {
-  const [showClickedMember, setShowClickedMember] = React.useState([
-    TeamArray[0],
-  ]);
-  const [teamMemberArray, setTeamMemberArray] = React.useState(TeamArray);
-  console.log(TeamArray);
+  const [teamMemberArray, setTeamMemberArray] = React.useState([]);
+  const [allTeamMemberArray, setAllTeamMemberArray] = React.useState([]);
+  const [showClickedMember, setShowClickedMember] = React.useState({
+    name: "",
+    image: "",
+    email: "",
+    description: "",
+    facebook: "",
+    medium: "",
+    instagram: "",
+    twitter: "",
+  });
+
+  React.useEffect(() => {
+    getFireStoreCounselor();
+  }, []);
+
+  const getFireStoreCounselor = async () => {
+    const teamArray = [];
+    await db
+      .collection("team_member")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          const data = doc.data();
+          teamArray.push({
+            id: doc.id,
+            name: data.name,
+            image: data.image,
+            email: data.email,
+            description: data.description,
+            facebook: data.facebook_link,
+            medium: data.medium_link,
+            instagram: data.insta_link,
+            twitter: data.twitter_link,
+          });
+        });
+      });
+    setShowClickedMember(teamArray[0]);
+    setTeamMemberArray(teamArray);
+    setAllTeamMemberArray(teamArray);
+  };
+
+  console.log(showClickedMember.name);
 
   const handleClick = (id: string) => {
-    const selectedMember = TeamArray.find((data) => data.id === id);
-    setShowClickedMember([selectedMember]);
+    const selectedMember = teamMemberArray.find((data) => data.id === id);
+    setShowClickedMember(selectedMember);
 
-    const filterTeamMemberArray = TeamArray.filter((data) => data.id !== id);
+    const filterTeamMemberArray = allTeamMemberArray.filter(
+      (data) => data.id !== id
+    );
     setTeamMemberArray(filterTeamMemberArray);
   };
-  console.log(teamMemberArray);
 
-  // console.log(showClickedMember);
   return (
     <div className="teams-list">
       <div className="teams-list__inner">
@@ -30,50 +76,46 @@ const Teams = () => {
         </div>
         <div className="teams-list__details">
           <div className="teams-list__left">
-            {showClickedMember &&
-              showClickedMember.map((member) => {
-                return (
-                  <div className="teams-list__thumbnail lead">
-                    <img src={member.img} alt="" />
-                  </div>
-                );
-              })}
+            <div className="teams-list__thumbnail lead">
+              <img src={showClickedMember.image} alt="member_logo" />
+            </div>
           </div>
           <div className="teams-list__right">
-            {showClickedMember &&
-              showClickedMember.map((member) => {
-                return (
-                  <>
-                    <div className="teams-list__md">
-                      <div className="teams-list__md__name">
-                        <span>{member.name}</span>
-                      </div>
-                      <div className="teams-list__designation">
-                        <span>{member.designation1}</span>
-                        <span>{member.designation2}</span>
-                      </div>
-                      <div className="teams-list__desc">
-                        {member.description}
-                      </div>
-                    </div>
-                    <div className="teams-list__contact">
-                      <div className="teams-list__email">
-                        <div className="teams-list__email__icon"></div>
-                        <div className="teams-list__email__address">
-                          {member.email}
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                );
-              })}
+            <div className="teams-list__md">
+              <div className="teams-list__md__name">
+                <span>{showClickedMember.name}</span>
+              </div>
+              <div className="teams-list__designation">
+                <span>Owner</span>
+                <span>Leader</span>
+              </div>
+              <div className="teams-list__desc">
+                {showClickedMember.description}
+              </div>
+            </div>
+            <div className="teams-list__contact">
+              <div className="teams-list__email">
+                <div className="teams-list__email__icon"></div>
+                <div className="teams-list__email__address">
+                  {showClickedMember.email}
+
+                  <div className="teams-list__socailmediaicons">
+                    <img src={twitterIcon} alt="twitter_logo" />
+                    <img src={facebookIcon} alt="facebook_logo" />
+                    <img src={instagramIcon} alt="instagram_logo" />
+                    <img src={youtubeIcon} alt="youtube_logo" />
+                    <img src={mIcon} alt="m_logo" />
+                  </div>
+                </div>
+              </div>
+            </div>
 
             <div className="teams-list__full-team">
               {teamMemberArray &&
                 teamMemberArray.map((TeamArray: any) => {
                   return (
                     <TeamMemberList
-                      image={TeamArray.img}
+                      image={TeamArray.image}
                       id={TeamArray.id}
                       handleClick={handleClick}
                     />

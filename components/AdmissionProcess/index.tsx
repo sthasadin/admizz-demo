19d0e59bd5a16fca51913d5c28723f531663e396
AanandBhandari <aanandbhandari143@gmail.com>
2021-04-18 10:17:10 +0545

    refactor admission process

--- components/AdmissionProcess/index.tsx
+++ components/AdmissionProcess/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 import { useSelector } from "react-redux";
 import moment from "moment";
 import schlorship from "../../public/scholarship.png";
@@ -12,23 +12,26 @@ const AdmissionProcess = () => {
     (state) => state.college.college?.scholarships
   );
 
+  console.log(admission)
   {
     return (
       <div id="admission" className="admission-process">
         <div className="admission-process__inner">
+          {admission && <Fragment>
           <div className="admission-process__title">Admission Process</div>
           <div className="admission-process__heading">{admission?.title}</div>
           <div className="admission-process__start">
             {/* Application Open for Jan Session: */}
             <span>
               {" "}
-              Start Date: {moment(admission?.start).format("DD MMM YYYY")} / End
-              Date: {moment(admission?.end).format("DD MMM YYYY")}
+              Start Date: {moment(admission?.start_date).format("DD MMM YYYY")} / End
+              Date: {moment(admission?.end_date).format("DD MMM YYYY")}
             </span>
           </div>
           <div className="admission-process__desc">
             {admission?.description}
           </div>
+          </Fragment>}
           <div className="admission-process__heading">Scholarship Options</div>
           <div className="admission-process__list">
             {scholarships ? (

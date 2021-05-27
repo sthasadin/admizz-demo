dd0a0a0e297087d3ab2e86d475b76369d995ace2
rodinshrestha <rodinshrestha@gmail.com>
2021-05-27 17:13:59 +0545

    design fixes in slider, colleg details

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -4,32 +4,33 @@ import { Card1 } from "../card/card1";
 const About = () => {
   return (
     <div className="about-country">
-      {/* <div className="container" style={{ border: "1px solid black" }}> */}
-      <div className="about-country__heading block-heading">about india</div>
-      <div className="about-country__title block-title">
-        learn something about india
-      </div>
-      <div className="about-country__desc">
-        <p>
-          India has traditionally been seen as a country that sends rather than
-          receives international students, but a large number of students from
-          elsewhere are now choosing to study in India, with the country
-          welcoming more than 30,000 international students every year.
-        </p>
-      </div>
-      <div className="about-country__info">
-        India has over <span>300,000 mosques</span> and over{" "}
-        <span>2 million Hindu temples</span>
-      </div>
-      <div className="about-country__thumbnail-wrap">
-        <Card1 />
-        <Card1 />
-        <Card1 />
-        <Card1 />
-        <Card1 />
+      <div className="inner__about">
+        {/* <div className="container" style={{ border: "1px solid black" }}> */}
+        <div className="about-country__heading block-heading">about india</div>
+        <div className="about-country__title block-title">
+          learn something about india
+        </div>
+        <div className="about-country__desc">
+          <p>
+            India has traditionally been seen as a country that sends rather
+            than receives international students, but a large number of students
+            from elsewhere are now choosing to study in India, with the country
+            welcoming more than 30,000 international students every year.
+          </p>
+        </div>
+        <div className="about-country__info">
+          India has over <span>300,000 mosques</span> and over{" "}
+          <span>2 million Hindu temples</span>
+        </div>
+        <div className="about-country__thumbnail-wrap">
+          <Card1 />
+          <Card1 />
+          <Card1 />
+          <Card1 />
+          <Card1 />
+        </div>
       </div>
     </div>
-    // </div>
   );
 };
 
--- layouts/topbar.tsx
+++ layouts/topbar.tsx
@@ -5,49 +5,55 @@ import React from "react";
 import Link from "next/link";
 import { auth } from "../firebase";
 
-
 const Topbar = (props: any) => {
   return (
     <div className="topbar">
-      <div className="topbar__contact">
-        <div className="contact">
-          <div className="contact__icon">
-            <FontAwesomeIcon icon={faMicrophoneAlt} />
-          </div>
-          <div className="contact__item">
-            <span className="contact__title">Nepal:</span>
-            <span className="contact__info">+977 87654321</span>
-          </div>
-          <div className="contact__item">
-            <span className="contact__title">India:</span>
-            <span className="contact__info">+91 87654321</span>
+      <div className="topbar__inner">
+        <div className="topbar__contact">
+          <div className="contact">
+            <div className="contact__icon">
+              {/* <FontAwesomeIcon icon={faMicrophoneAlt} /> */}
+              <img src="/contactIcon.png" alt="contact-us" />
+            </div>
+            <div className="contact__item">
+              <span className="contact__title">Nepal:</span>
+              <span className="contact__info">+977 87654321</span>
+            </div>
+            <div className="contact__item">
+              <span className="contact__title">India:</span>
+              <span className="contact__info">+91 87654321</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="topbar__right">
-        <div className="topbar__country">
-          <Link href="/login">
-            <span>Study In</span>
-            {/* <Select /> */}
-          </Link>
-        </div>
+        <div className="topbar__right">
+          <div className="topbar__country">
+            <Link href="/login">
+              <span>Study In</span>
+              {/* <Select /> */}
+            </Link>
+          </div>
 
-        <div className="topbar__signup">
-          {!auth.currentUser ? <div className="signup">
-            <div className="signup__icon">
-              <FontAwesomeIcon icon={faUserTie} />
-            </div>
-            <div className="signup__text">
-              <Link href="/register">Sign up</Link>
-            </div>
-          </div> : <div className="signup">
-            <div className="signup__icon">
-              <FontAwesomeIcon icon={faUserTie} />
-            </div>
-            <div className="signup__text">
-              <Link href="/studentdashboardmain">Student Dashboard</Link>
-            </div>
-          </div>}
+          <div className="topbar__signup">
+            {!auth.currentUser ? (
+              <div className="signup">
+                <div className="signup__icon">
+                  <FontAwesomeIcon icon={faUserTie} />
+                </div>
+                <div className="signup__text">
+                  <Link href="/register">Sign up</Link>
+                </div>
+              </div>
+            ) : (
+              <div className="signup">
+                <div className="signup__icon">
+                  <FontAwesomeIcon icon={faUserTie} />
+                </div>
+                <div className="signup__text">
+                  <Link href="/studentdashboardmain">Student Dashboard</Link>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>

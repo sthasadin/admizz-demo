560aa675bddf80e9684cc11f332b0dbe186350c5
rodinshrestha <rodinshrestha@gmail.com>
2021-07-25 19:15:07 +0545

    college detial facuilty issue fixed

--- components/FacilitiesOnCollege/facilityItem.tsx
+++ components/FacilitiesOnCollege/facilityItem.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const FacilityItem = (props: any) => {
   return (
     <div className="facility-item">
-      <div className="facility-item__icon">{props.icon}</div>
+      <div className="facility-item__icon">
+        <img src={props.icon} alt="..." />
+      </div>
       <div className="facility-item__label">{props.label}</div>
     </div>
   );

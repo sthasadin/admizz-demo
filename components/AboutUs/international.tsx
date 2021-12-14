4c4d6d77708f3f884d91a3af7764d2069eba1dc4
Bisikha <bisikhasubedi26@gmail.com>
2021-12-14 07:58:12 +0545

    testimonial fixes

--- components/AboutUs/international.tsx
+++ components/AboutUs/international.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 const international = () => {
     return (
         <div className="our-exclusive-container">
-        <div className="our-exclusive-content">
-          <div className="our-trusted-text">We are international</div>
-          <div className="our-exclusive-title">We Have Served Student’s From 50+ Countries</div>
-          <div className="international-map">
-              <img src="/grey-world-map.png"/>
-          </div>
+            <div className="aboutus-exclusive-content">
+                <div className="our-trusted-text">We are international</div>
+                <div className="aboutus-exclusive-title">We Have Served Student’s From 50+ Countries</div>
+                <div className="international-map">
+                </div>
+            </div>
+            <div className="aboutus-testimonial">
+                <div className="testimonial-exclusive-content">
+                    <div className="our-trusted-text">TESTIMONIALS</div>
+                    <div className="aboutus-exclusive-title">What our universities are saying about us.</div>
+                </div>
+            </div>
         </div>
-    </div>
     )
 }
 

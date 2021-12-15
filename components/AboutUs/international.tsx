b6053e481aaa0923d3acbc24d7db565dff384168
Bisikha <bisikhasubedi26@gmail.com>
2021-12-15 16:00:01 +0545

    Slider in Testimonial

--- components/AboutUs/international.tsx
+++ components/AboutUs/international.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import Slider from "react-slick";
 
 const international = () => {
+    var sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    }
     return (
         <div className="our-exclusive-container">
             <div className="aboutus-exclusive-content">
@@ -14,13 +22,29 @@ const international = () => {
                     <div className="our-trusted-text">TESTIMONIALS</div>
                     <div className="aboutus-exclusive-title">What our universities are saying about us.</div>
                     <div className="aboutus-testimonial-wrapper">
-                        <div className="aboutus-testimonial-content">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
-                            Morbi ornare vulputate nulla, non vestibulum nisi. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
-                            Morbi ornare vulputate nulla, non vestibulum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Sed eu turpis pulvinar justo dictum blandit eget vel diam. Morbi ornare vulputate nulla, non vestibulum nisi.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        </div>
+                        <Slider {...sliderSettings}>
+                            <div className="aboutus-testimonial-content">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
+                                Morbi ornare vulputate nulla, non vestibulum nisi. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
+                                Morbi ornare vulputate nulla, non vestibulum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Sed eu turpis pulvinar justo dictum blandit eget vel diam. Morbi ornare vulputate nulla, non vestibulum nisi.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            </div>
+                            <div className="aboutus-testimonial-content">
+                                Slider Test Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
+                                Morbi ornare vulputate nulla, non vestibulum nisi. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
+                                Morbi ornare vulputate nulla, non vestibulum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Sed eu turpis pulvinar justo dictum blandit eget vel diam. Morbi ornare vulputate nulla, non vestibulum nisi.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            </div>
+                            <div className="aboutus-testimonial-content">
+                                Slider Test Again Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
+                                Morbi ornare vulputate nulla, non vestibulum nisi. Sed eu turpis pulvinar justo dictum blandit eget vel diam. 
+                                Morbi ornare vulputate nulla, non vestibulum nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Sed eu turpis pulvinar justo dictum blandit eget vel diam. Morbi ornare vulputate nulla, non vestibulum nisi.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            </div>
+                        </Slider>
                     </div>
                 </div>
             </div>

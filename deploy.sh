724cde28653bc43ce8f5f92d1fbc1ffcde97e350
= <wazzree@gmail.com>
2022-03-29 14:00:12 +0545

    file changed

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,10 @@ cd /AdmizzWeb
 echo "Update app from Git"
 git pull origin master
 echo "Yarn and build"
+rm -rf node_modules yarn.lock
 yarn
 yarn build
 echo "Run new PM2 action"
 pm2 restart AdmizzWeb
+echo "node version"
+node --version

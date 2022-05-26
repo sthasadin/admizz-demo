9b45bd8d08d4844d987f18e94db2138659062742
Asmitaadhikarii <asmita.adhikari@asterdio.com>
2022-05-26 10:39:35 +0545

    Typeerror

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,7 @@ if (typeof window !== "undefined") {
     NProgress.done();
   });
 }
+const PProvider:any=Provider
 const MyApp = ({ Component, pageProps }: AppProps) => {
   React.useEffect(() => {
     Router.events.on("routeChangeComplete", () => {
@@ -37,9 +38,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
     <AuthProvider>
-      <Provider store={store}>
+      <PProvider store={store}>
         <Component {...pageProps} />
-      </Provider>
+      </PProvider>
     </AuthProvider>
     </>
   );

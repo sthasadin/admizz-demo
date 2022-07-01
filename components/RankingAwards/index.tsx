d482b3cfbf2056d7e81f27b62ed02b1c753ad723
Rajan Midun Magar <rajan.magar@asterdio.com>
2022-07-01 16:03:27 +0545

    ranking solved

--- components/RankingAwards/index.tsx
+++ components/RankingAwards/index.tsx
@@ -5,38 +5,52 @@ import { useSelector } from "react-redux";
 const index = () => {
   const college = useSelector((state: any) => state.college.college);
   if(college){
-    const ranks=[];
-    for(let i=0;i<4;i++){
+    // FOR LAST 4 YEARS
+    const firstYear=moment().subtract(3, "year").year();
+    const secondYear=moment().subtract(2, "year").year();
+    const thirdYear=moment().subtract(1, "year").year();
+    const fourthYear=moment().subtract(0, "year").year();
+      
+    // TO STORE ONLY RANK TITLE, LOGO AND RANK VALUE
+    const rankTitles=[];
+    college?.college_rankings?.map((rank,index)=>{
+      const object={
+        title:rank.rank_id.title,
+        logo:rank.rank_id.logo,
+        ranks:['NA','NA','NA','NA']
+      }
+      if(index == 0) rankTitles.push(object)
+      else{
+        let count =0
+        rankTitles.map(rankTitle=>{
+          if((rankTitle.title).toLowerCase() == (object.title).toLowerCase())
+            count++;
+        })
+        if(count==0)
+          rankTitles.push(object)
+      }
+    })
+
+    rankTitles.map(rank => {
       let count=0;
-      college?.college_rankings?.map(rank=>{
-        if(i==0){
-          if(rank.year == moment().subtract(3, "year")){
-            ranks.push(rank.rank);
-            count++;
-          }
+      college?.college_rankings?.map((data) => {
+        if ((rank.title).toLowerCase() == (data.rank_id.title).toLowerCase()) {
+          if (Number(data.year) == firstYear)
+            rank.ranks[0] = data.rank;
+          else if (Number(data.year) == secondYear)
+            rank.ranks[1] = data.rank;
+          else if (Number(data.year) == thirdYear)
+            rank.ranks[2] = data.rank;
+          else if (Number(data.year) == fourthYear)
+            rank.ranks[3] = data.rank;
+          count++;
         }
-        else if(i==1){
-          if(rank.year==moment().subtract(2, "year").year()){
-            ranks.push(rank.rank);
-            count++;
-          }
-        }
-        else if(i==2){
-          if(rank.year==moment().subtract(1, "year").year()){
-            ranks.push(rank.rank);
-            count++;
-          }
-        }
-        else{
-          if(rank.year==moment().subtract(0, "year").year()){
-            ranks.push(rank.rank);
-            count++;
-          }
-        }
-      });
-      if(count==0)
-        ranks.push('NA')
-    }
+      })
+      if(count ==college.college_rankings.length)
+        return rank;
+    })
+
+    console.log(college?.college_rankings);
 
     return (
       <div>
@@ -53,27 +67,32 @@ const index = () => {
               <div>{moment().subtract(1, "year").year()}</div>
               <div>{moment().subtract(0, "year").year()}</div>
             </div>
-            <div className="rankingawards__awardscontainer">
-              <div className="rankingawards__awardsbox">
-                <div className="rankingawards_contents">
-                  <div>
-                    <img
-                      src={college?.college_rankings?.[0]?.rank_id.logo}
-                      style={{ maxHeight: "80px", maxWidth: "80px", width: "80px" }}
-                    />
+            {
+              rankTitles.map(rank=>{
+                return (<div className="rankingawards__awardscontainer">
+                  <div className="rankingawards__awardsbox">
+                    <div className="rankingawards_contents">
+                      <div>
+                        <img
+                          src={rank.logo}
+                          style={{ maxHeight: "80px", maxWidth: "80px", width: "80px" }}
+                        />
+                      </div>
+                      {
+                        rank.ranks.map((rank1,index)=>{
+                          return (
+                              <div style={{ color: "#FFA200" }} key={index}>
+                                {rank1}
+                              </div>
+                          )
+                        })
+                      }
+                    </div>
                   </div>
-                  {
-                    ranks.map((rank,index)=>{
-                      return (
-                          <div style={{ color: "#FFA200" }} key={index}>
-                            {rank}
-                          </div>
-                      )
-                    })
-                  }
                 </div>
-              </div>
-            </div>
+                )
+              })
+            }
             {/* </>
         ) : null} */}
             {college.college_awards?.length ? (
